cd203bd971b749d7b20499963fb562036c11d7f0
Sergey Konovalov <konovalov20004@gmail.com>
2023-05-31 17:21:03 +0300

    2done

---
+++ Seminar2/2/2.py
@@ -0,0 +1,19 @@
+a = []
+b = int(input())
+c = 0
+for i in range(b):
+    c += 1
+    a.append(c)
+
+print(a)
+x = int(input())
+c = 0
+for i in a:
+    if x == i - 1 or x == i + 1:
+        c += 1
+        print(i)
+    
+if c == 0 and max(a) < x:
+    print(max(a))
+else :
+    print(min(a))
